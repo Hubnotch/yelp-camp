afa8ac9a16874b014673b66772511a901998d52e
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-08-11 21:36:56 +0100

    Added login authentiction using passport

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require('../models/user')
+const asyncErrorHandler = require('../utils/asyncErrorHandler')
 
-router.get('/register', (req,res)=>{
+//Render registration form
+router.get('/register', (req, res) => {
     res.render('users/register')
 })
+
+//Handle registration form
+router.post('/register', asyncErrorHandler(async (req, res) => {
+    try {
+        const { email, username, password } = req.body
+        const user = new User({ email, username })
+        const newUser = await User.register(user, password)
+        console.log(newUser)
+        req.flash('success', 'Welcome to Yelp camp')
+        res.redirect('/campgrounds')
+    } catch (error) {
+        req.flash('error', error.message)
+        res.redirect('/register')
+    }
+}))
+
+//Render login page
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+//Handle user logging in
+router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}), (req, res) => {
+    req.flash('success', 'Welcome back')
+    res.redirect('/campgrounds')
+})
+
 
 
 module.exports = router
