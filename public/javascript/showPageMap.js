dd8a020ac4b7387c15b0b244e77b58926cdfabbd
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-08-14 08:31:02 +0100

    Added map from mapbox

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,7 +1,13 @@
+const campground = require("../../models/campground");
+
 mapboxql.accessToken = mapToken;
 const map = new mapboxql.Map({
-    contianer:'map',
-    style:'mapbox://styles/mapbox/streets-v11',
-    center: [-79.0, 47],
-    zoom:9
+    contianer: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: campground.geometry.coordinates,
+    zoom: 9
 })
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
